c7e53eb3a4ef0317f966e61d19d1895cdf438eb4
Harsh <harshjindal955@gmail.com>
2026-02-05 23:43:24 +0530

    add some functionality in the Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,9 +7,9 @@ interface ButtonProps {
   // text: string;
   children: React.ReactNode;
   // startIcon: ReactElement;
-  // onClick?: () => void;
-  // fullWidth?: boolean;
-  // loading?: boolean;
+  onClick?: () => void;
+  fullWidth?: boolean;
+  loading?: boolean;
   // // It is best practice to allow a className prop for external overrides
   className?: string;
 }
@@ -24,15 +24,15 @@ export function Button({
   // text,  // not using anymore
   children, // • Allows icons, spans, strong tags, etc. • More reusable
   // startIcon,
-  // onClick,
-  // fullWidth,
-  // loading,
+  onClick,
+  fullWidth,
+  loading,
   className,
 }: ButtonProps) {
   return (
     <button
-      // onClick={onClick}
-      // disabled={loading}
+      onClick={onClick}
+      disabled={loading}
       className={cn(
         // 1. Base styles (always applied)
         "flex cursor-pointer items-center justify-center gap-1.5 rounded-md px-4 py-2 font-normal drop-shadow-md drop-shadow-slate-400 transition-colors text-shadow-md",
@@ -44,10 +44,10 @@ export function Button({
         // If fullWidth is true, these are added.
         // 'flex' and 'items-center' are redundant here as they are in Base,
         // but 'justify-center' is new. cn() handles this cleanly.
-        // fullWidth && "w-full justify-center",
+        fullWidth && "w-full justify-center",
 
         // Loading state
-        // loading && "cursor-not-allowed opacity-45",
+        loading && "cursor-not-allowed opacity-45",
 
         // 4. External overrides (allows you to pass className="m-4" to the component)
         className,
