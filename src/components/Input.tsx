8b55ff9643d9e4f09fdf5dd0c46f46ea9b72a2e2
Harsh <harshjindal955@gmail.com>
2026-03-15 10:58:29 +0530

    add error styling for the Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,31 @@
-interface InputProps {
-  placeholder: string;
-  reference?: any;
+import * as React from "react";
+import { forwardRef } from "react";
+import { cn } from "../lib/utils";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
 }
 
-export const Input = ({ placeholder, reference }: InputProps) => {
-  return (
-    <div>
-      <input
-        type="text"
-        ref={reference}
-        placeholder={placeholder}
-        className="m-2 rounded border px-4 py-2"
-      />
-    </div>
-  );
-};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, className, ...props }, ref) => {
+    return (
+      <div className="flex flex-col gap-1">
+        <input
+          ref={ref}
+          className={cn(
+            "m-2 rounded border px-4 py-2 transition-colors outline-none",
+            error
+              ? "border-red-500 focus:border-red-500"
+              : "border-gray-300 focus:border-gray-500",
+            className,
+          )}
+          {...props}
+        />
+
+        {error && <p className="ml-2 text-sm text-red-500">{error}</p>}
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
